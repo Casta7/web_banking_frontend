84050509bd389083a61b7f993e75e99257342f9b
Enz079 <zheng.enzo@itismeucci.com>
2026-05-11 09:34:00 +0200

    navabar and routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { Deposito } from "./components/deposito/deposito";
+import { ListaMovimenti } from "./components/lista-movimenti/lista-movimenti";
+import { Preleva } from "./components/preleva/preleva";
+import { Saldo } from "./components/saldo/saldo";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect alla home all'avvio
+  { path: 'home', component: ListaMovimenti },
+  { path: 'preleva', component: Preleva },
+  { path: 'saldo', component: Saldo },
+  { path: 'deposito', component: Deposito },
+  { path: '**', redirectTo: '/home' } // Wildcard: se la rotta non esiste torna in home
+];
